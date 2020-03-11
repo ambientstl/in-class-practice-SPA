3ca99ed5381014a60a5c469bdd8ea27b4c22310c
ambientstl <41842065+ambientstl@users.noreply.github.com>
2020-03-11 12:30:19 -0500

    update form to accept url and description for new gallery pic

--- components/Main.js
+++ components/Main.js
@@ -11,17 +11,14 @@ export default () => `<section id="jumbotron">
 </section>
 <section id="gallery"></section>
 <form id="register" method="POST" action="">
-<div>
-  <label for="username">Username:</label>
-  <input type="text" name="username" id="username" placeholder="Enter Your Username">
-</div>
-<div>
-  <label for="password">Password:</label>
-  <input type="password" name="password" id="password">
-</div>
-<div>
-  <label for="email">Email:</label>
-  <input type="email" name="email" id="email" placeholder="your@email.here">
-</div>
-<input type="submit" name="register" value="Register">
-</form>`
+  <h2> Add a photo to the gallery!</h2>
+  <div>
+    <label for="url">Photo URL:</label>
+    <input type="text" name="url" id="url" placeholder="Enter Photo URL" required>
+  </div>
+  <div>
+    <label for="title">Photo Title/Description:</label>
+    <input type="text" name="title" id="title" placeholder="Enter Photo Description" required>
+  </div>
+  <input type="submit" name="submit" value="Submit Photo">
+</form>`;
